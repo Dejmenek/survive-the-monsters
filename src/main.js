adcc5557513c39296ca2e81baa1f53c26db4e748
Dejmenek <dejmen14567@gmail.com>
2024-11-05 17:39:12 +0100

    fix: update logo and phaser.js path

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import Phaser from "phaser";
+import Phaser from "../node_modules/phaser/dist/phaser.js";
 import Bootloader from "./scenes/bootloader.js";
 import Splash from "./scenes/splash.js";
 import Game from "./scenes/game.js";
